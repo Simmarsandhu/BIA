d08a7c314f588a62f436f632844918fe0ca9d2d0
Simmarsandhu <77791732+Simmarsandhu@users.noreply.github.com>
2023-05-07 22:14:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var el_up =
+document.getElementById(
+"divheading2");
+var el_down =
+document.getElementById(
+"divheading2");
+function GFG_Fun() {
+var x = 370;
+var y = 250;
+var el = document.getElementById("divheading2");
+el.style.position = "absolute";
+el.style.left = x + 'px';
+el.style.top = y + 'px';
+ 
+}
+window.onload(GFG_fun());
